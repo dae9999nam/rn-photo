1a476d9c41295c027b81370ad634d9bc949d77c3
Seunghyeon Baek <shyun@Shyun.local>
2024-02-10 19:30:35 +0800

    second commit

---
+++ src/screens/SignInScreen.js
@@ -0,0 +1,7 @@
+import { useNavigation } from '@react-navigation/native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { AuthRoutes } from '../navigations/routes';
+
+const SignInScreen = () => {
+  const navigation = useNavigation();
+};
