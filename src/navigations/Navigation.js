b3e70a91ec1ac2eb075c7b801c1566fdfdb042f8
Seunghyeon Baek <u3582165@connect.hku.hk>
2024-02-22 15:55:29 +0800

    Fourty Nineth Commit

--- src/navigations/Navigation.js
+++ src/navigations/Navigation.js
@@ -4,8 +4,11 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useEffect, useState } from 'react';
 import { Asset } from 'expo-asset';
 import { initFirebase } from '../api/firebase';
+import { useUserState } from '../contexts/USerContext';
+import MainStack from './MainStack';
 
 const Navigation = () => {
+  const [user] = useUserState();
   const [isReady, SetISReady] = useState(false);
 
   useEffect(() => {
@@ -37,7 +40,7 @@ const Navigation = () => {
 
   return (
     <NavigationContainer onReady={onReady}>
-      <AuthStack />
+      {user.uid ? <MainStack /> : <AuthStack />}
     </NavigationContainer>
   );
 };
